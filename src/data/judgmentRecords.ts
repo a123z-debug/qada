607d5d9ca3b2b24c4c9febd10fe6a225dd546f9a
a123z-debug <azxcvbvcxz1@gmail.com>
2026-09-19 15:32:32 +0300

    Make WARSHA AI branch an isolated static app

--- src/data/judgmentRecords.ts
+++
@@ -1,4 +0,0 @@
-import { JudgmentRecord } from '../types';
-
-// قائمة فارغة: تم مسح القضايا التجريبية لتبدأ المنصة بسجل نظيف لكل مستخدم
-export const INITIAL_JUDGMENT_RECORDS: JudgmentRecord[] = [];
